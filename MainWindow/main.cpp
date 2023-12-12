4815167c9c4bb16a91f291b9eb04aaddaa18a6b8
Sania <Sania@DESKTOP-3HL58UN>
2023-12-12 21:13:09 +0800

    Main Window in process

---
+++ MainWindow/main.cpp
@@ -0,0 +1,80 @@
+﻿#include<Windows.h>
+#include"resource.h"
+
+CONST CHAR g_sz_WINDOW_CLASS[] = "My first Window";
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+	//1) Регистрация класса окна:
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+	//cb - Count Bytes отвечают за размеры окна
+	wc.cbSize = sizeof(WNDCLASSEX);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.style = 0;
+	//внешний вид окна
+	//wc.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	//wc.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	wc.hIcon = (HICON)LoadImage(hInstance, "icon2.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);//на панели задач
+	wc.hIconSm = (HICON)LoadImage(hInstance, "icon1.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);//в строке заголовка окна
+	wc.hCursor = (HCURSOR)LoadImage(hInstance, "Betamon.ani", IMAGE_CURSOR, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wc.hCursor = LoadCursor(hInstance, MAKEINTRESOURCE(IDC_CURSOR1));
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	//
+	wc.hInstance = hInstance;
+	wc.lpszMenuName = NULL;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+	wc.lpszClassName = g_sz_WINDOW_CLASS;
+	//регистрируем
+	if (RegisterClassEx(&wc) == NULL)
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
+	}
+
+	//2) Создание окна:
+	HWND hwnd = CreateWindowExA
+	(
+		NULL,//ExStyles
+		g_sz_WINDOW_CLASS,//Class name
+		g_sz_WINDOW_CLASS,//Заголовок окна
+		WS_OVERLAPPEDWINDOW,//Стиль для главного окна:
+							//наличие строки заголовка, кнопки управления 
+							//и масштабирование размеров
+		CW_USEDEFAULT, CW_USEDEFAULT,//положение окна на экране
+		CW_USEDEFAULT, CW_USEDEFAULT,//размер окна
+		NULL,	//родительское окно (у главного нет)
+		NULL,	//для главного окна задает меню,
+				//для дочернего окна ID-ресурса дочернего окна (IDC_BUTTON_COPY, IDC_EDIT...)
+		hInstance,
+		NULL
+	);
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
+	//3) Запуск цикла сообщений:
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_COMMAND:
+		break;
+	case WM_DESTROY:PostQuitMessage(0); break;
+	case WM_CLOSE:  DestroyWindow(hwnd); break;
+	default:		return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return 0;
+}
