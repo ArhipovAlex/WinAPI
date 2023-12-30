c56f7095229578000df0e271a0c47018e8a71488
Sania <Sania@DESKTOP-3HL58UN>
2023-12-30 21:21:40 +0800

    Text Load Done

---
+++ TextEditor/main.cpp
@@ -0,0 +1,183 @@
+﻿#include<Windows.h>
+#include<Richedit.h>
+#include"resource.h"
+
+CONST CHAR g_sz_CLASS_NAME[] = "My Text Editor";
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+INT CALLBACK DlgProcAbout(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+BOOL LoadTextFileToEdit(HWND hEdit, LPCSTR lpszFileName);
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+//1) Регистрация класса окна
+	WNDCLASSEX wc;
+	ZeroMemory(&wc, sizeof(wc));
+	//стиль окна
+	wc.cbSize = sizeof(wc);
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.style = 0;
+	//внешний вид окна
+	wc.hIcon = (HICON)LoadImage(hInstance, "Icons\\text_editor_icon.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wc.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
+	wc.hIconSm = (HICON)LoadImage(hInstance, "Icons\\text_editor_icon.ico", IMAGE_ICON, LR_DEFAULTSIZE, LR_DEFAULTSIZE, LR_LOADFROMFILE);
+	//wc.hIconSm = LoadIcon(hInstance, IDI_APPLICATION);
+	wc.hCursor = LoadCursor(hInstance, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	//привязка названия окна, основной процедуры CALLBACK и основного меню
+	wc.hInstance = hInstance;
+	wc.lpszClassName = g_sz_CLASS_NAME;
+	wc.lpfnWndProc = (WNDPROC)WndProc;
+	wc.lpszMenuName = MAKEINTRESOURCE(IDR_MAIN_MENU);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(NULL, "Class registration failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+//2) Создание окна
+	HWND hwnd = CreateWindowEx
+	(
+		NULL,
+		g_sz_CLASS_NAME,
+		g_sz_CLASS_NAME,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+	if (hwnd == NULL)
+	{
+		MessageBox(NULL, "Window creation failed", "Error", MB_OK | MB_ICONERROR);
+		return 0;
+	}
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+//3) Запуск цикла сообщений
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_CREATE:
+	{
+		LoadLibrary("riched20.dll");
+		RECT rect;
+		GetWindowRect(hwnd, &rect);
+		HWND hEdit = CreateWindowEx
+		(
+			NULL,RICHEDIT_CLASS, "",
+			WS_CHILD|WS_VISIBLE|ES_MULTILINE|ES_AUTOHSCROLL|ES_AUTOVSCROLL,
+			0,0,
+			rect.right,rect.bottom,
+			hwnd,
+			(HMENU)IDC_EDIT,
+			GetModuleHandle(NULL),
+			NULL
+		);
+	}
+		break;
+	case WM_SIZE:
+	{
+		RECT rect;
+		GetClientRect(hwnd, &rect);
+		SetWindowPos(GetDlgItem(hwnd, IDC_EDIT), NULL, rect.left, rect.top, rect.right, rect.bottom,SWP_NOZORDER);
+	}
+		break;
+	case WM_COMMAND:
+		switch (LOWORD(wParam))
+		{
+		case ID_FILE_OPEN:
+		{
+			CHAR szFileName[MAX_PATH]{};
+
+			OPENFILENAME ofn;
+			ZeroMemory(&ofn, sizeof(ofn));
+
+			ofn.lStructSize = sizeof(ofn);
+			ofn.hwndOwner = hwnd;
+			ofn.lpstrFilter = "Text files (*.txt)\0*.txt\0All files (*.*)\0*.*\0";
+			ofn.lpstrFile = szFileName;
+			ofn.nMaxFile = MAX_PATH;
+			ofn.Flags = OFN_EXPLORER|OFN_FILEMUSTEXIST|OFN_HIDEREADONLY;
+			ofn.lpstrDefExt = "txt";
+
+			if (GetOpenFileName(&ofn))
+			{
+				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+				LoadTextFileToEdit(hEdit, szFileName);
+				//DWORD dwTextLenght = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
+				//LPSTR lpszFileContent = (LPSTR)GlobalAlloc(GPTR, dwTextLenght + 1);
+				//SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)lpszFileContent);
+			}
+		}
+		break;
+		case ID_HELP_ABOUT:
+			DialogBoxParam(GetModuleHandle(NULL), MAKEINTRESOURCE(IDD_DIALOG_ABOUT), hwnd, DlgProcAbout, 0);
+		break;
+		}
+		break;
+	case WM_DESTROY: PostQuitMessage(0);break;
+	case WM_CLOSE:DestroyWindow(hwnd); break;
+	default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}
+	return FALSE;
+}
+
+INT CALLBACK DlgProcAbout(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:
+		break;
+	case WM_COMMAND:
+
+		switch (LOWORD(wParam))
+		{
+		case IDOK:EndDialog(hwnd, 0);
+		}
+	case WM_CLOSE:EndDialog(hwnd, 0);
+	}
+	return FALSE;
+}
+
+BOOL LoadTextFileToEdit(HWND hEdit, LPCSTR lpszFileName)
+{
+	BOOL bSuccess = FALSE;
+	HANDLE hFile = CreateFile(lpszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+	if (hFile != INVALID_HANDLE_VALUE)
+	{
+		DWORD dwFileSize = GetFileSize(hFile, NULL);
+		if (dwFileSize != UINT_MAX)
+		{
+			LPSTR lpszFileText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
+			if (lpszFileText)
+			{
+				DWORD dwRead;
+				if (ReadFile(hFile, lpszFileText, dwFileSize, &dwRead, NULL))
+				{
+					lpszFileText[dwFileSize] = 0;
+					if (SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)lpszFileText))bSuccess = TRUE;
+				}
+				GlobalFree(hFile);
+			}
+			CloseHandle(hFile);
+		}
+	}
+	return bSuccess;
+}
