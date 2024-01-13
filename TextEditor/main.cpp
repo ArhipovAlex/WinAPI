bec24e474d1744545f1b3ff3d55a39bf5f19e5db
Sania <Sania@DESKTOP-3HL58UN>
2024-01-13 17:13:32 +0800

    New and Open DONE

--- TextEditor/main.cpp
+++ TextEditor/main.cpp
@@ -2,6 +2,7 @@
 #include<Richedit.h>
 #include"resource.h"
 
+//CHAR Buffer[8 * 1024 * 1024];
 CONST CHAR g_sz_CLASS_NAME[] = "My Text Editor";
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -9,6 +10,8 @@ INT CALLBACK DlgProcAbout(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 BOOL LoadTextFileToEdit(HWND hEdit, LPCSTR lpszFileName);
 BOOL SaveTextFileFromEdit(HWND hEdit, LPCSTR lpszFileName);
+void LoadFile(HWND hEdit, LPCSTR lpszFileName);
+BOOL SavingThisFile(HWND hwnd, BOOL onSave);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -109,9 +112,17 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
-
+		case ID_FILE_NEW:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			onSave = SavingThisFile(hwnd, onSave);
+			SendMessage(hEdit, EM_GETMODIFY, FALSE, 0);
+			SendMessage(hEdit, WM_SETTEXT, WPARAM(0), LPARAM(""));
+		}
+		break;
 		case ID_FILE_OPEN:
 		{
+			onSave=SavingThisFile(hwnd, onSave);
 			//CHAR szFileName[MAX_PATH]{};
 			OPENFILENAME ofn;
 			ZeroMemory(&ofn, sizeof(ofn));
@@ -126,11 +137,11 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (GetOpenFileName(&ofn))
 			{
 				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
-				onSave=LoadTextFileToEdit(hEdit, szFileName);
+				//onSave=LoadTextFileToEdit(hEdit, szFileName);
 				//DWORD dwTextLenght = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
 				//LPSTR lpszFileContent = (LPSTR)GlobalAlloc(GPTR, dwTextLenght + 1);
 				//SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)lpszFileContent);
-				
+				LoadTextFileToEdit(hEdit, szFileName);
 			}
 		}
 		break;
@@ -256,3 +267,63 @@ BOOL SaveTextFileFromEdit(HWND hEdit, LPCSTR lpszFileName)
 	delete[] data;
 	return TRUE;
 }
+
+BOOL SavingThisFile(HWND hwnd, BOOL onSave)
+{
+	if (SendMessage(GetDlgItem(hwnd, IDC_EDIT), EM_GETMODIFY, 0, 0))
+	{
+		onSave = FALSE;
+		int message = MessageBox(NULL, "Вы хотите сохранить изменения в файл?", g_sz_CLASS_NAME, MB_YESNO | MB_ICONQUESTION);
+		switch (message)
+		{
+		case IDYES:
+		{
+			SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVE, 0);
+			break;
+		}
+		case IDNO:break;
+		}
+	}
+	return onSave;
+}
+/*
+void LoadFile(HWND hEdit, LPCSTR lpszFileName)
+{
+	BYTE BUFFERSIZE = 8 * 1024 * 1024;
+	// объявление переменной для получения указателя на выделенную память
+	LPVOID pBuffer;
+	// объявление переменной для хэндла файла
+	HANDLE hFile;
+	// объявление переменной для получения количества прочитанных байт
+	DWORD dwHaveRead;
+	// объявление переменной для получения результата функции чтения файла
+	BOOL blResult;
+	// динамическое выделение памяти нужного объЁма
+	pBuffer = VirtualAlloc(NULL, BUFFERSIZE, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	// проверка, что память успешно выделена
+	if (pBuffer != NULL)
+	{
+		// открытие заведомо существующего файла с доступом только на чтение,
+		// общим доступом тоже только на чтение и с указанием для ОС, что файл
+		// будет последовательно считываться (это необходимо для лучшего кэширования)
+		hFile = CreateFile(lpszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL | FILE_FLAG_SEQUENTIAL_SCAN, NULL);
+
+		// проверка, что файл успешно открыт
+		if (hFile != INVALID_HANDLE_VALUE)
+		{
+			// цикл чтения файла кусками
+			do
+			{
+				// чтение файла
+				blResult = ReadFile(hFile, pBuffer, BUFFERSIZE, &dwHaveRead, NULL);
+				SetWindowTextA(hEdit, Buffer);
+			} while (blResult && (dwHaveRead == BUFFERSIZE));
+
+			// закрытие файла
+			CloseHandle(hFile);
+		}
+
+		// освобождение выделенной памяти
+		VirtualFree(pBuffer, 0, MEM_RELEASE);
+	}
+}*/
